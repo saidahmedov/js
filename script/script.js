4f692dac0533a31bc1db2b6e89b99eacf9eebdb7
Said Ahmedov <89637007721@mail.ru>
2019-08-23 17:12:05 +0300

    Второй урок

--- script/script.js
+++ script/script.js
@@ -1,7 +1,40 @@
+/* Первый урок
 var money, income, addExpenses, deposit, mission, period;
 money = 157;
 income = 250;
 period = "Второй";
 alert( money );
 alert(period);
-console.log('income: ', income);
+console.log('income: ', income); */
+
+
+let money = 70000;
+console.log(typeof money);
+
+income = '150000';
+console.log(typeof income);
+console.log(income.length);
+
+period = 7;
+console.log(period);
+
+mission = 350000;
+console.log(mission);
+
+addExpenses = 'КОМПЬЮТЕР, МАШИНА, КВАРТИРА';
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+deposit = true;
+
+let budgetDay = 70000;
+console.log(budgetDay / 30);
+
+
+
+
+
+// for (let i = 1; i < 13; i++) {
+// 	console.log(i);
+// }
+
