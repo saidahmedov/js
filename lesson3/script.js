45f84e3a7984ffbde2da20775b8242d36c304a97
Said Ahmedov <89637007721@mail.ru>
2019-08-27 00:45:57 +0300

    третий урок

---
+++ lesson3/script.js
@@ -0,0 +1,48 @@
+let mission = 350000;
+console.log(mission);
+
+let money = prompt('Ваш ежемесячный доход?');
+console.log(money);
+console.log(typeof money);
+
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.');
+console.log([addExpenses]);
+
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
+console.log(typeof deposit);
+
+income = '150000';
+console.log(typeof income);
+
+
+let costs = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+console.log(costs);
+let costs2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+console.log(costs2);
+let HowMuchCost = prompt('Во сколько это обойдется?');
+console.log(HowMuchCost);
+let HowMuchCost2 = prompt('Во сколько это обойдется?');
+console.log(HowMuchCost2);
+ let budgetMonth = money - costs;
+console.log(budgetMonth);
+console.log(Math.ceil(mission / budgetMonth ));
+
+
+
+let budgetDay = prompt('Введите ваш ежедневный доход');
+	if (budgetDay > 800) {
+		console.log('Высокий уровень дохода');
+	}
+	else if ((budgetDay > 300) && (budgetDay < 800)){
+		console.log('Средний уровень дохода');
+	}
+	else if ((budgetDay > 0) && (budgetDay < 300)) {
+		console.log('Низкий уровень дохода');
+	} 
+	else if (budgetDay < 0)  {
+		console.log('Что то пошло не так');
+	}
+	 else {
+		console.log('Что то пошло не так');
+	}
