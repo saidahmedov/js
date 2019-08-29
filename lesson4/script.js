b60c29b88ff9a3391fe40a59a8aab89626bf1439
Said Ahmedov <89637007721@mail.ru>
2019-08-29 12:55:22 +0300

    Четвертый урок

---
+++ lesson4/script.js
@@ -0,0 +1,65 @@
+let mission = 350000;
+console.log(mission);
+let income = '150000';
+
+let money = +prompt('Ваш ежемесячный доход?', 100000);
+console.log(money);
+
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.', 'Еда, еда и еще раз еда))');
+console.log([addExpenses]);
+
+let deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
+
+let showTypeOf = function (data) {
+	console.log(data, typeof(data));
+}
+showTypeOf(money);
+showTypeOf(deposit);
+showTypeOf(income);
+
+let costs = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+console.log(costs);
+let howMuchCost = +prompt('Во сколько это обойдется?', 3500);
+console.log(howMuchCost);
+let costs2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+console.log(costs2);
+let howMuchCost2 = +prompt('Во сколько это обойдется?', 2000);
+console.log(howMuchCost2);
+let budgetMonth = money - howMuchCost - howMuchCost2;
+console.log(budgetMonth);
+console.log(Math.ceil(mission / budgetMonth ));
+console.log(Math.floor(budgetMonth / 30));
+
+let budgetDay = Math.floor(budgetMonth / 30);
+
+let getStatusIncome = function (){
+	if (budgetDay >= 800){
+		return('Высокий уровень дохода');
+	}else if ((budgetDay >= 300) && (budgetDay < 800)){
+	 return('Средний уровень дохода');
+	}else if ((budgetDay >= 0) && (budgetDay < 300)){
+	 return('Низкий уровень дохода');
+	}else {
+	 return('Что то пошло не так');
+	}
+   };
+   	console.log(getStatusIncome());
+
+
+	function getExpensesMonth(a, b) {
+		return a + b;
+	};
+	let result = getExpensesMonth(howMuchCost, howMuchCost2);
+		console.log(result);
+
+	let accumulatedMonth = function getAccumulatedMonth(data) {
+		console.log(data);
+	};
+		accumulatedMonth(money - result);
+
+	function getTargetMonth(c, d) {
+		return c / d;
+	}
+	let missionForMoth = getTargetMonth(mission, budgetMonth);
+		console.log(Math.floor(missionForMoth));
